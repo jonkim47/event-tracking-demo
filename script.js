a531aa0e228704a2e62693f186413ef372389a9f
Jonathan <jonkim47@gmail.com>
2023-08-07 10:03:07 -0400

    tons of changes from local. login modal

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Smooth scrolling for navigation links
+    const openModalButton = document.querySelector('.open-modal-button');
+    const modal = document.querySelector('.modal');
+    const closeModalButton = document.querySelector('.close-modal-button');
+    const modalForm = document.getElementById('modal-form');
+
+    const logoutButton = document.querySelector('.logout-button');
+
     const navLinks = document.querySelectorAll('nav a');
-    navLinks.forEach(link => {
-        link.addEventListener('click', smoothScroll);
-    });
-
-    function smoothScroll(event) {
-        event.preventDefault();
-        const targetId = this.getAttribute('href');
-        const target = document.querySelector(targetId);
-        window.scrollTo({
-            top: target.offsetTop - 100,
-            behavior: 'smooth'
-        });
-    }
-
-    // Slide-in animations for sections
+    
     const sections = document.querySelectorAll('.slide-in');
     const slideOptions = {
         threshold: 0.5
@@ -34,27 +26,53 @@ document.addEventListener('DOMContentLoaded', function() {
         slideObserver.observe(section);
     });
 
-    // Toggle user menu
-    const userMenu = document.querySelector('.user-menu');
-    const userMenuToggle = document.querySelector('.user-menu-toggle');
-    const loginButton = document.querySelector('.login-button');
-    const logoutButton = document.querySelector('.logout-button');
-
-    userMenuToggle.addEventListener('click', function() {
-        userMenu.classList.toggle('show');
+    navLinks.forEach(link => {
+        link.addEventListener('click', smoothScroll);
     });
 
-    // Handle login button
-    loginButton.addEventListener('click', function(event) {
+    function smoothScroll(event) {
         event.preventDefault();
-        // Add your login functionality here
-        alert('Login clicked');
-    });
+        const targetId = this.getAttribute('href');
+        const target = document.querySelector(targetId);
+        window.scrollTo({
+            top: target.offsetTop - 100,
+            behavior: 'smooth'
+        });
+    }
 
-    // Handle logout button
     logoutButton.addEventListener('click', function(event) {
         event.preventDefault();
         // Add your logout functionality here
         alert('Logout clicked');
     });
+
+    openModalButton.addEventListener('click', function() {
+        modal.style.display = 'block';
+    });
+
+    closeModalButton.addEventListener('click', function() {
+        modal.style.display = 'none';
+    });
+
+    modalForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const name = document.getElementById('name').value;
+        const contact = document.getElementById('contact').value;
+        
+        // Perform tracking or any other action you need
+        trackLogin(name, contact);
+
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', function(event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+
+    function trackLogin(name, contact) {
+        // Replace this with your actual tracking code or other actions
+        console.log(`Logged in: Name - ${name}, Contact - ${contact}`);
+    }
 });
