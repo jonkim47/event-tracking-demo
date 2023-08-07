79386ccbac8055ea5b982989134bfce3162d06af
Jonathan <jonkim47@gmail.com>
2023-08-07 14:50:29 -0400

    Login Sorta Works

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Smooth scrolling for navigation links
+    const navLinks = document.querySelectorAll('nav a');
+    
     const openModalButton = document.querySelector('.open-modal-button');
     const modal = document.querySelector('.modal');
     const closeModalButton = document.querySelector('.close-modal-button');
     const modalForm = document.getElementById('modal-form');
 
     const logoutButton = document.querySelector('.logout-button');
-
-    const navLinks = document.querySelectorAll('nav a');
+    const loginButton = document.getElementById('login-button');  
     
+    navLinks.forEach(link => {
+        link.addEventListener('click', smoothScroll);
+    });
+
+    function smoothScroll(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href');
+        const target = document.querySelector(targetId);
+        window.scrollTo({
+            top: target.offsetTop - 100,
+            behavior: 'smooth'
+        });
+    }
+
+    // Slide-in animations for sections
     const sections = document.querySelectorAll('.slide-in');
     const slideOptions = {
         threshold: 0.5
@@ -26,30 +43,21 @@ document.addEventListener('DOMContentLoaded', function() {
         slideObserver.observe(section);
     });
 
-    navLinks.forEach(link => {
-        link.addEventListener('click', smoothScroll);
-    });
-
-    function smoothScroll(event) {
-        event.preventDefault();
-        const targetId = this.getAttribute('href');
-        const target = document.querySelector(targetId);
-        window.scrollTo({
-            top: target.offsetTop - 100,
-            behavior: 'smooth'
-        });
-    }
-
+    // Handle logout button
     logoutButton.addEventListener('click', function(event) {
         event.preventDefault();
-        // Add your logout functionality here
-        alert('Logout clicked');
+        logout();
+        updateLoginButton();
     });
 
+    // Handle opening the modal when the "Login" button is clicked
     openModalButton.addEventListener('click', function() {
-        modal.style.display = 'block';
+        if (!isLoggedIn()) {
+            modal.style.display = 'block';
+        }
     });
 
+    // Handle closing the modal
     closeModalButton.addEventListener('click', function() {
         modal.style.display = 'none';
     });
@@ -57,10 +65,9 @@ document.addEventListener('DOMContentLoaded', function() {
     modalForm.addEventListener('submit', function(event) {
         event.preventDefault();
         const name = document.getElementById('name').value;
-        const contact = document.getElementById('contact').value;
-        
-        // Perform tracking or any other action you need
-        trackLogin(name, contact);
+        const email = document.getElementById('email').value;
+
+        login(name, email); // Call the login function to log the user in
 
         modal.style.display = 'none';
     });
@@ -71,8 +78,44 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    function trackLogin(name, contact) {
-        // Replace this with your actual tracking code or other actions
-        console.log(`Logged in: Name - ${name}, Contact - ${contact}`);
+    function isLoggedIn() {
+        return localStorage.getItem('userLoggedIn') === 'true';
     }
+
+    function login(name, email) {
+        localStorage.setItem('userLoggedIn', 'true');
+        localStorage.setItem('userEmail', email);
+        updateLoginButton();
+    }
+
+    function logout() {
+        localStorage.removeItem('userLoggedIn');
+        localStorage.removeItem('userEmail');
+        updateLoginButton();
+    }
+
+    function trackLogin(name, email) {
+        // Replace this with your actual tracking code or other actions
+        analytics.identify("97980cfea0067", {
+            name: name,
+            email: email,
+            plan: "premium",
+            logins: 5
+        });
+
+        console.log(`Logged in: Name - ${name}, Email - ${email}`);
+    }
+
+    function updateLoginButton() {
+        if (isLoggedIn()) {
+            loginButton.classList.add('logged-in');
+            loginButton.textContent = 'Logged In';
+        } else {
+            loginButton.classList.remove('logged-in');
+            loginButton.textContent = 'Login';
+        }
+    }
+
+    // Check login status on page load
+    updateLoginButton();
 });
