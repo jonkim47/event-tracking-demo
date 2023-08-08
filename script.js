81c4292095192804341f3c7f402e728082beeb7a
Jonathan <jonkim47@gmail.com>
2023-08-08 00:01:39 -0400

    major updates

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
+let loginButton;
+
 document.addEventListener('DOMContentLoaded', function() {
     // Smooth scrolling for navigation links
     const navLinks = document.querySelectorAll('nav a');
     
+    // Handle logout button
+    const logoutButton = document.querySelector('.logout-button');
+    logoutButton.addEventListener('click', handleLogout);
+
+    // Assign the loginButton value inside the event listener
+    loginButton = document.getElementById('login-button');
+    loginButton.addEventListener('click', openLoginModal);
+
+    // Handle login modal functionality
     const openModalButton = document.querySelector('.open-modal-button');
-    const modal = document.querySelector('.modal');
-    const closeModalButton = document.querySelector('.close-modal-button');
+    const closeModalButton = document.querySelector('.login-modal .close-modal-button'); 
+    const modal = document.querySelector('.login-modal');
     const modalForm = document.getElementById('modal-form');
 
-    const logoutButton = document.querySelector('.logout-button');
-    const loginButton = document.getElementById('login-button');  
-    
-    navLinks.forEach(link => {
-        link.addEventListener('click', smoothScroll);
-    });
+    openModalButton.addEventListener('click', openLoginModal); 
+    closeModalButton.addEventListener('click', closeLoginModal); 
+    modalForm.addEventListener('submit', handleModalFormSubmission);
 
-    function smoothScroll(event) {
-        event.preventDefault();
-        const targetId = this.getAttribute('href');
-        const target = document.querySelector(targetId);
-        window.scrollTo({
-            top: target.offsetTop - 100,
-            behavior: 'smooth'
-        });
-    }
+    // Handle contact modal functionality
+    const contactModalButton = document.querySelector('.open-contact-modal-button');
+    const closeContactModalButton = document.querySelector('.contact-modal .close-modal-button');
+    const contactModal = document.querySelector('.contact-modal');
+    const contactModalForm = document.getElementById('contact-modal-form');
+
+    contactModalButton.addEventListener('click', openContactModal);
+    closeContactModalButton.addEventListener('click', closeContactModal);
+    contactModalForm.addEventListener('submit', handleContactFormSubmission);
 
     // Slide-in animations for sections
     const sections = document.querySelectorAll('.slide-in');
@@ -43,76 +51,128 @@ document.addEventListener('DOMContentLoaded', function() {
         slideObserver.observe(section);
     });
 
-    // Handle logout button
-    logoutButton.addEventListener('click', function(event) {
-        event.preventDefault();
-        logout();
-        updateLoginButton();
-    });
-
-    // Handle opening the modal when the "Login" button is clicked
-    openModalButton.addEventListener('click', function() {
-        if (!isLoggedIn()) {
-            modal.style.display = 'block';
-        }
-    });
-
-    // Handle closing the modal
-    closeModalButton.addEventListener('click', function() {
-        modal.style.display = 'none';
-    });
-
-    modalForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        const name = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-
-        login(name, email); // Call the login function to log the user in
-        trackLogin(name, email);
-
-        modal.style.display = 'none';
-    });
-
-    window.addEventListener('click', function(event) {
-        if (event.target === modal) {
-            modal.style.display = 'none';
-        }
-    });
-
     // Product modals functionality
-
     const productButtons = document.querySelectorAll('.product-button');
     const productModal = document.querySelector('.product-modal');
     const productDetailsContainer = document.querySelector('.product-details');
+    const closeProductModalButton = productModal.querySelector('.close-modal-button');
 
-    productButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            const productId = this.getAttribute('data-product');
-            const productDetails = getProductDetails(productId); // Replace with your own function to get product details
+    // Functions
 
-            // Populate the modal content with product details
-            productDetailsContainer.innerHTML = `
-                <h3>${productDetails.name}</h3>
-                <p>${productDetails.description}</p>
-                <img id="product-image" src="${productDetails.image}" alt="Product Image">
-                <!-- Add more product details here -->
-            `;
+    function handleModalFormSubmission(event) {
+        event.preventDefault();
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        login(name, email);
+        trackLogin(name, email);
+        closeLoginModal();
+    }
 
-            // Show the product modal
-            productModal.style.display = 'block';
-        });
-    });
+    function handleContactFormSubmission(event) {
+        event.preventDefault();
+        const name = document.getElementById('contact-name').value;
+        const email = document.getElementById('contact-email').value;
+        const phone = document.getElementById('contact-phone').value;
+        const jobTitle = document.getElementById('contact-job-title').value;
+        const company = document.getElementById('contact-company').value;
 
-    const closeProductModalButton = document.querySelector('.close-product-modal-button');
-    closeProductModalButton.addEventListener('click', function() {
-        productModal.style.display = 'none';
-    });
+        // Process the form data and show a thank you message
+        closeContactModal();
+        alert('Thank you for contacting us!');
+    }
 
-    window.addEventListener('click', function(event) {
-        if (event.target === productModal) {
-            productModal.style.display = 'none';
+    // Handle logout button
+    function handleLogout(event) {
+        event.preventDefault();
+        logout();
+        updateLoginButton();
+    }
+
+    // Handle modal functionality
+    function openLoginModal() {
+        if (!isLoggedIn()) {
+            modal.style.display = 'block';
+        } else {
+            alert('You are already logged in.');
         }
-    });
+    }
+
+    function closeLoginModal() {
+        modal.style.display = 'none';
+    }
+
+    // Contact Modal functions
+    function openContactModal() {
+        contactModal.style.display = 'block';
+    }
+
+    function closeContactModal() {
+        contactModal.style.display = 'none';
+    }
+
+    // Product modals functionality
+    function openProductModal() {
+        const productId = this.getAttribute('data-product');
+        const productDetails = getProductDetails(productId); 
+
+        // Populate the modal content with product details
+        productDetailsContainer.innerHTML = `
+            <h3>${productDetails.name}</h3>
+            <p>${productDetails.description}</p>
+            <img id="product-image" src="${productDetails.image}" alt="Product Image">
+            <!-- Add more product details here -->
+        `;
+
+        // Show the product modal
+        productModal.style.display = 'block';
+
+        // Add event listener to the close button inside the product modal
+        closeProductModalButton.addEventListener('click', closeProductModal);
+    }
+
+    function closeProductModal() {
+        productModal.style.display = 'none';
+    }
+
+    // Additional functions for login functionality
+    // ...
+
+    function isLoggedIn() {
+        return localStorage.getItem('userLoggedIn') === 'true';
+    }
+
+    function login(name, email) {
+        localStorage.setItem('userLoggedIn', 'true');
+        localStorage.setItem('userEmail', email);
+        updateLoginButton();
+    }
+
+    function logout() {
+        localStorage.removeItem('userLoggedIn');
+        localStorage.removeItem('userEmail');
+        analytics.reset();
+        updateLoginButton();
+    }
+
+    function trackLogin(name, email) {
+        // Replace this with your actual tracking code or other actions
+        analytics.identify(email, {
+            name: name,
+            email: email
+        });
+
+        console.log(`Logged in: Name - ${name}, Email - ${email}`);
+    }
+
+    function updateLoginButton() {
+        if (isLoggedIn()) {
+            loginButton.classList.add('logged-in');
+            loginButton.textContent = 'Logged In';
+        } else {
+            loginButton.classList.remove('logged-in');
+            loginButton.textContent = 'Login';
+        }
+    }
 
     function getProductDetails(productId) {
         // Example data for demonstration purposes
@@ -163,47 +223,11 @@ document.addEventListener('DOMContentLoaded', function() {
     
         return products[productId];
     }
-    
 
-
-    // Login Functionality
-
-    function isLoggedIn() {
-        return localStorage.getItem('userLoggedIn') === 'true';
-    }
-
-    function login(name, email) {
-        localStorage.setItem('userLoggedIn', 'true');
-        localStorage.setItem('userEmail', email);
-        updateLoginButton();
-    }
-
-    function logout() {
-        localStorage.removeItem('userLoggedIn');
-        localStorage.removeItem('userEmail');
-        analytics.reset();
-        updateLoginButton();
-    }
-
-    function trackLogin(name, email) {
-        // Replace this with your actual tracking code or other actions
-        analytics.identify(email, {
-            name: name,
-            email: email
-        });
-
-        console.log(`Logged in: Name - ${name}, Email - ${email}`);
-    }
-
-    function updateLoginButton() {
-        if (isLoggedIn()) {
-            loginButton.classList.add('logged-in');
-            loginButton.textContent = 'Logged In';
-        } else {
-            loginButton.classList.remove('logged-in');
-            loginButton.textContent = 'Login';
-        }
-    }
+    // Add event listeners for product buttons
+    productButtons.forEach(button => {
+        button.addEventListener('click', openProductModal);
+    });
 
     // Check login status on page load
     updateLoginButton();
